f09d21a3432b4eafbcfc21ab270b23f5ab8c1988
Sharath Raj <sharathrj143@gmail.com>
2024-01-15 22:42:18 +0530

    Added Admin Areas and ViewModels Added Categories controller and View

---
+++ FastFood.Web/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,88 @@
+﻿using FastFood.Models;
+using FastFood.Repository;
+using FastFood.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace FastFood.Web.Areas.Admin.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var listOfData = _context.Categories.ToList().Select(x => new CategoryViewModel()
+            {
+                Id = x.Id,
+                Title = x.Title,
+            }).ToList();
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            CategoryViewModel category = new CategoryViewModel();
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Create(CategoryViewModel categoryViewModel)
+        {
+            Category model = new Category();
+            model.Title = categoryViewModel.Title;
+            _context.Categories.Add(model);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var viewModel = _context.Categories.Where(x => x.Id == id).Select(
+                x => new CategoryViewModel()
+                {
+                    Id = x.Id,
+                    Title = x.Title
+                }).FirstOrDefault();
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(CategoryViewModel categoryViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var categoryFromDb = _context.Categories.FirstOrDefault(x => x.Id == categoryViewModel.Id);
+                if (categoryFromDb != null)
+                {
+                    categoryFromDb.Title = categoryViewModel.Title;
+                    _context.Categories.Update(categoryFromDb);
+                    _context.SaveChanges();
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var category = _context.Categories.Where(x => x.Id == id).FirstOrDefault();
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ FastFood.Web/Models/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FastFood.Web.Models.ViewModels
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
